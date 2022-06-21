bb88bba1279ab98b3b6d98e764bbb95c0f5e591b
Andrea Santamaria Garcia <andrea.santamaria@kit.edu>
2022-06-21 17:47:08 +0200

    Saving the output to file

--- extract_ff.py
+++ extract_ff.py
@@ -1,6 +1,17 @@
 import numpy as np
 import subprocess
 
+def run_fixfield():
+    print ('Running fixfield.exe...')
+    # output = subprocess.check_output(["./fixfield.exe"], shell=True)
+
+    bashCommand = './../fixfield-source/fixfield.exe'
+    # bashCommand = './fixfield.exe -1 1.15 1.31 0 0'
+    outfile = open('output.txt', 'w')
+    process = subprocess.Popen(bashCommand.split(), stdout=outfile)
+    output, error = process.communicate()
+    outfile.close()
+
 def co_transfermatrix():
     """Run fixfield.exe and return the computed closed orbit
     and linear (coupled) transfer matrix.
@@ -10,13 +21,11 @@ def co_transfermatrix():
        - transfer matrix np.array
     """
 
-    print ('Running fixfield.exe...')
-    output = subprocess.check_output(["./fixfield.exe"], shell=True)
-    print ('Extracting results...')
-
     nl = False
 
-    for line in str(output).split('\\n'):
+    file = open("output.txt", "r")
+    output = file.read()
+    for line in str(output).split('\n'):
         if 'Closed orbit found:' in line:
             x_clo = float(line.split('x_clo = ')[1].split(' [m]')[0])
             xprime_clo = float(line.split('xprime_clo = ')[1].split(' [deg]')[0])
@@ -29,9 +38,9 @@ def co_transfermatrix():
             break
 
     transfer_matrix = str(output).split(
-        "--------------------------------- Get matrix first order -------------------------------------\\n")[1].split(
-        "\\nbias")[0]
+        "--------------------------------- Get matrix first order -------------------------------------\n")[1].split(
+        "\nbias")[0]
 
-    transfer_matrix = np.array([(l.split('\\t')[:-1]) for l in transfer_matrix.split('\\n')], dtype=float)
+    transfer_matrix = np.array([(l.split('\t')[:-1]) for l in transfer_matrix.split('\n')], dtype=float)
 
     return ((x_clo, xprime_clo, z_clo, zprime_clo), transfer_matrix)
